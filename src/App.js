597c331c2aa1633d798699551ea581444eaa1aad
ing. Julio Cuneo <jucuneo@gmail.com>
2020-10-22 18:27:31 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+const { ipcRenderer } = window.require("electron");
 
 function App() {
+  const [replay, setReplay] = useState(null);
+
+  useEffect(() => {
+    ipcRenderer.on("replay", (event, arg) => {
+      setReplay(arg);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button
+          onClick={() => {
+            ipcRenderer.send("asynchronous-message", "ping");
+          }}
+        >
+          Com
+        </button>
+        <div>{replay}</div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
